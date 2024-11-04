1f2df6bd972e0349dff86b3430379148b5e2ac1c
mihutka <misabajandinov@gmail.com>
2024-11-04 11:24:28 +0200

    finally 2

--- index.js
+++ index.js
@@ -1,85 +1,76 @@
 const express = require('express');
 const cors = require('cors');
-const { error } = require('console');
 const { type } = require('os');
 const app = express();
 
 app.use(cors());
-app.use(express.json()); 
+app.use(express.json());
 
-const widgets =
-[
-    {id : 1, name : "Super Mario Bros. Wonder"},
-    {id : 2, name : "Animal Crossing"},
-    {id : 3, name : "Seiklus"},
+const games = [
+    {id: 1, name: "Super Mario Bros. Wonder"},
+    {id: 2, name: "Animal Crosing: New Horizons"},
+    {id: 3, name: "Seiklus"},
 
 ]
 
 
-//get method, returns one game from array, by id. If id doesn't exist
-//return statuscode 404 - not found =>
-
-
-app.get('/games/:id',(req,res) =>
+app.get('/games/', (req,res) => 
 {
     res.send(games)
 })
 
 
+//get method, returns one game from array by id, if id doesnt exits returns 404
 
-
-app.get('/games', (req,rest) =>
+app.get('/games/:id', (req, res) =>
 {
     if (typeof games[req.param.id - 1] === 'undefined')
     {
-        return res.status(404).send({error: "Game not found, game not gaming"})
+        return res.status(404).send({error: "Game not found"});
     }
-
-    res.send(games[req.params.id-1])
+    res.send(games[req.param.Id - 1])
 })
 
 
 
-
-app.post('/games', (req,res) => {
-if(!req.body.name || !req.body.price) {
-    return res.status(400).send({error: 'One or multiple parameters missing'});
-
-}
-let newGame = {
-    id: games.lenght + 1,
-    price: req.body.price,
-    name: req.body.name
-}
-games.push(newGame)
-res.status(201).location('localhost:8000/games/' + (games.lenght -1)).send(newGame)
+app.post('/games', (req, res) => {
+    if (!req.body.name || !req.body.price) {
+        return res.status(404).send(
+            {error: "One or multiple paramaters missing"});
+    }
+    let newGame = {
+        id: games.length +1,
+        price: req.body.price,
+        name: req.body.name
+    }
+    games.push(newGame)
+    res.status(201).location('localhost:8080/games/'
+     + (games.length - 1)).send(newGame)
 })
 
 
-//delete a game, where id is specified, if game is not found,
-//returns statuscode 4
-//d
-
+//deletes a game, where id is specified
+//if game isnt found returns 404
+//othwerise success 204 no content
 app.delete('/games/:id', (req, res) =>
 {
-   if (typeof games[req.params.id] - 1 === 'undefined')
-   {
-    return res.status(404).send(
-        {error: 'Game not found, no gaming for you'}
-    )
-   }
-   games.splice(req.params.id -1, 1)
-   res.status(204).send({error: 'Content not contenting (no content)'})
-}
-)
+    if(typeof games[req.params.id - 1] === 'undefined')
+    {
+        return res.status(404).send(
+            {error: 'Game not found, no gaming for you'})
+    }
+    games.splice(req.params.id -1, 1)
+    res.status(204).send({error: 'Content not contening (no content)'})
 
-
-app.put('/games', (req, res) => {
-    res.send("Oh no")
 })
 
-//define the adress upon which the app is running
+//app.put
+app.put('/games', (req, res) => {
+    res.send("PUT Request ")
+})
 
-app.listen (8080, () =>
-console.log('Api töötab aadressil: http://localhost:8080')
+app.listen(8080, () =>
+    console.log(`Api töötab aadressil: http://localhost:8080`)
 )
+
+
